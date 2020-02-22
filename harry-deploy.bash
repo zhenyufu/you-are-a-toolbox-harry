06edde06d251b3889bf7e723eafef9d38d896ea6
zhenyufu <fzysmile@gmail.com>
2020-02-21 18:29:03 -0800

    better deploy

--- harry-deploy.bash
+++ harry-deploy.bash
@@ -2,21 +2,25 @@
 
 echo "#### setting up vim"
 sudo apt-get install vim 
+
+echo "#### copying vimrc"
 if [ -f $HOME/.vimrc ]; then
     mv ~/.vimrc ~/vimrc_old
 fi
 cp config/vimrc ~/.vimrc
 
-
-#mv $HOME/.harry.bash $HOME/harry_old.bash
+echo "#### copying .harry.hash"
+if [ -f $HOME/.harr.bash ]; then
+    mv $HOME/.harry.bash $HOME/harry_old.bash
+fi
 cp config/harry.bash ~/.harry.bash
 
 
-## Add this to .bashrc to  include harry functions 
-#if [ -f $HOME/.harry.bash ]; then
-#    source $HOME/.harry.bash
-#fi
-
+echo "#### including .harry.bash to bashrc"
+sed -i '$a #### harry.bash' $HOME/.bashrc
+sed -i '$a if [ -f $HOME/.harry.bash ]; then' $HOME/.bashrc
+sed -i '$a \\tsource $HOME/.harry.bash' $HOME/.bashrc
+sed -i '$a fi' $HOME/.bashrc
 
 
 
